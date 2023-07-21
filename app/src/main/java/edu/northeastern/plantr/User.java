be5b1c836ce02abf0ab6c58a2d9cd7b60d9dd3bc
Almeida <kalmeida@northeastern.edu>
2023-07-21 16:27:09 -0400

    fixed appCompat and added some user fields

--- app/src/main/java/edu/northeastern/plantr/User.java
+++ app/src/main/java/edu/northeastern/plantr/User.java
@@ -1,16 +1,25 @@
 package edu.northeastern.plantr;
 
+import android.net.Uri;
+
 public class User {
     public String username;
     public String firstName;
     public String lastName;
+    public String favePlant;
+    public String lastActivity;
+    public Uri profPic;
 
     public User() {}
 
-    public User(String username, String firstName, String lastName) {
+    public User(String username, String firstName, String lastName,
+                String favePlant, String lastActivity, Uri profPic) {
 
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.favePlant = favePlant;
+        this.lastActivity = lastActivity;
+        this.profPic = profPic;
     }
 }
