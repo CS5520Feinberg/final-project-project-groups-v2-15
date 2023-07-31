0125d9dcb2070e8cab8344539f7555dfaf4e3e78
nolanmatt34 <nolan.matt@northeastern.edu>
2023-07-31 15:08:19 -0400

    Added Creating new plant dialog menu to type in name and species

--- app/src/main/java/edu/northeastern/plantr/MyPlantsActivity.java
+++ app/src/main/java/edu/northeastern/plantr/MyPlantsActivity.java
@@ -1,7 +1,12 @@
 package edu.northeastern.plantr;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.plantr.R;
@@ -13,6 +18,30 @@ public class MyPlantsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plantr_home_page);
 
+    }
+
+    protected void createNewPlant(String newName, String newSpecies, View view){
+        Plant newPlant = new Plant(newName, newSpecies);
+    }
+
+    public void fabAddPlantDialog(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MyPlantsActivity.this);
+        View v = getLayoutInflater().inflate(R.layout.fragment_dialog_layout, null);
+        builder.setView(v);
+        EditText txt_PlantNameInput = (EditText)v.findViewById(R.id.plantNameEnter);
+        EditText txt_PlantSpeciesInput = (EditText)v.findViewById(R.id.plantSpeciesEnter);
+        builder.setNegativeButton(R.string.cancel, (dialog, id) -> dialog.dismiss());
+        builder.setPositiveButton(R.string.ok, (DialogInterface.OnClickListener) (dialog, id) -> {
+            String plantName = txt_PlantNameInput.getText().toString();
+            String plantSpecies = txt_PlantSpeciesInput.getText().toString();
+            if(plantName.length() == 0 || plantSpecies.length() == 0){
+                Toast.makeText(MyPlantsActivity.this, "Please Enter Info", Toast.LENGTH_SHORT)
+                        .show();
+            }else{
+                createNewPlant(plantName, plantSpecies, view);
+            }
+        });
+        builder.show();
     }
 
 }
--- app/src/main/java/edu/northeastern/plantr/Plant.java
+++ app/src/main/java/edu/northeastern/plantr/Plant.java
@@ -6,13 +6,23 @@ import java.util.Date;
 
 public class Plant {
     public String plantName;
+    public String plantSpecies;
     public Uri plantPic;
     public Date[] wateredDates;
     public Integer[] heights;
 
-    public Plant(String name, Uri picture) {
+    public Plant(String name, String species, Uri picture) {
         this.plantName = name;
+        this.plantSpecies = species;
         this.plantPic = picture;
+        this.wateredDates = new Date[1];
+        this.heights = new Integer[1];
+    }
+
+    public Plant(String name, String species){
+        this.plantName = name;
+        this.plantSpecies = species;
+        this.plantPic = null;
         this.wateredDates = new Date[1];
         this.heights = new Integer[1];
     }
