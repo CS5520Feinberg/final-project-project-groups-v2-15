df8a811875eee9986868ceb8fa6ac0af454a8ff4
Almeida <kalmeida@northeastern.edu>
2023-08-01 21:16:05 -0400

    making fragments for the nav bar

---
+++ app/src/main/java/edu/northeastern/plantr/FragmentMyPlants.java
@@ -0,0 +1,22 @@
+package edu.northeastern.plantr;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMyPlants extends Fragment {
+
+    public FragmentMyPlants() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_plants, container, false);
+    }
+}
