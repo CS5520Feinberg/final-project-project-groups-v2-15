1243b596b57ab4b6950e7613b070857f7b56f781
nolanmatt34 <nolan.matt@northeastern.edu>
2023-08-04 18:01:01 -0400

    Added Water/Measure button and dialog to plant detail page

--- app/src/main/java/edu/northeastern/plantr/PlantDetails.java
+++ app/src/main/java/edu/northeastern/plantr/PlantDetails.java
@@ -1,11 +1,16 @@
 package edu.northeastern.plantr;
 
-import android.graphics.Bitmap;
+import android.content.DialogInterface;
+import static java.lang.Integer.parseInt;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.net.URI;
@@ -26,6 +31,23 @@ public class PlantDetails extends AppCompatActivity {
         waterComments = findViewById(R.id.waterText);
         Plant loadedPlant = (Plant)getIntent().getParcelableExtra("Plant");
         //setProfileDetails(loadedPlant);
+    }
+
+    public void AddWater(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(PlantDetails.this);
+        View v = getLayoutInflater().inflate(R.layout.fragment_water_layout, null);
+        builder.setView(v);
+        CheckBox waterCheck = (CheckBox)v.findViewById(R.id.waterCheck);
+        EditText heightUpdate = (EditText)v.findViewById(R.id.growthInput);
+        builder.setNegativeButton("@string/cancel", (dialog, id)-> dialog.dismiss());
+        builder.setPositiveButton("Update", (DialogInterface.OnClickListener) (dialog, id) -> {
+            Boolean watered = waterCheck.isChecked();
+            String plantHeight = heightUpdate.getText().toString();
+            int plantHeightInt = parseInt(plantHeight);
+        });
+
+        builder.show();
+
     }
 
     protected void setProfileDetails(Plant loadedPlant){
