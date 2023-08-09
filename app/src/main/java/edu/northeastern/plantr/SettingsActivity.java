f8969f6e1f0e2f9e15e1cb217eba73a75ae86e07
Almeida <kalmeida@northeastern.edu>
2023-08-08 21:58:13 -0400

    adding the dialogs for settings changes

--- app/src/main/java/edu/northeastern/plantr/SettingsActivity.java
+++ app/src/main/java/edu/northeastern/plantr/SettingsActivity.java
@@ -1,17 +1,26 @@
 package edu.northeastern.plantr;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private BottomNavigationView navBar;
+    private String favePlantName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +49,38 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void changeFavePlant(int position) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Edit Favorite Plant");
+
+        LinearLayout lay = new LinearLayout(this);
+        lay.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText plantInput = new EditText(this);
+        plantInput.setHint("Plant Name");
+        plantInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        lay.addView(plantInput);
+
+        dialog.setView(lay);
+
+        dialog.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                favePlantName = plantInput.getText().toString();
+                //TODO >>>actually change the plant name here<<<
+
+                Snackbar.make(SettingsActivity.this.getCurrentFocus(),
+                        "Information successfully updated.", Snackbar.LENGTH_LONG).show();
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        dialog.show();
     }
 }
